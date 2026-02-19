47008c27b792fd93c01ef2d9da0e81a0a19fe8ea
pennycuda <141153875+pennycuda@users.noreply.github.com>
2026-02-19 14:36:23 -0500

    fix secret key path

--- sennet/integrated_maps_ui/production_settings.py
+++ sennet/integrated_maps_ui/production_settings.py
@@ -4,7 +4,7 @@ print("Loading production settings")
 
 DEBUG = False
 
-SECRET_PATH = Path("/opt/secret")
+SECRET_PATH = Path("/opt/integrated-maps-ui/secret")
 # Static secret key is required
 with open(SECRET_PATH / "django_secret_key.txt") as f:
     SECRET_KEY = f.read().strip()
