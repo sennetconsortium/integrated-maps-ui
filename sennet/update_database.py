69a2657e787bd6bc88ee2926493326e58a68442d
pennycuda <141153875+pennycuda@users.noreply.github.com>
2026-02-20 14:14:54 -0500

    update language for db

--- sennet/update_database.py
+++ sennet/update_database.py
@@ -15,7 +15,7 @@ from pathlib import Path
 def change_dataset_mapping():
     datasets = Dataset.objects.order_by("sntid")
     for ds in datasets:
-        data_prod = ds.data_product
+        data_prod = ds.integrated_map
         data_prod.dataSets.add(ds)
 
 def main():
