436dbc4339cb50f050f1201e4ec9d455a98c6921
pennycuda <141153875+pennycuda@users.noreply.github.com>
2026-02-20 12:18:58 -0500

    set debug to true

--- sennet/integrated_maps_ui/production_settings.py
+++ sennet/integrated_maps_ui/production_settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 print("Loading production settings")
 
-DEBUG = False
+DEBUG = True
 
 SECRET_PATH = Path("/opt/integrated-maps-ui/secret")
 # Static secret key is required
