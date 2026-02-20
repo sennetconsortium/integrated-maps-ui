d08d700c940bd35cad23471de63d399bad102b15
pennycuda <141153875+pennycuda@users.noreply.github.com>
2026-02-20 17:28:23 -0500

    syntax error

--- sennet/integrated_maps_ui/settings.py
+++ sennet/integrated_maps_ui/settings.py
@@ -133,7 +133,7 @@ STATIC_ROOT = BASE_DIR / "static"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "/opt/media/"
-MEDIA_ROOT = "opt/media"/
+MEDIA_ROOT = "opt/media/"
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
