60390b80d04d6a37d5be86ebe83ac82c7af40034
pennycuda <141153875+pennycuda@users.noreply.github.com>
2026-03-02 15:33:04 -0500

    add allowed hosts

--- sennet/integrated_maps_ui/production_settings.py
+++ sennet/integrated_maps_ui/production_settings.py
@@ -10,9 +10,7 @@ with open(SECRET_PATH / "django_secret_key.txt") as f:
     SECRET_KEY = f.read().strip()
 
 ALLOWED_HOSTS = [
-    "https://sn-integrated-mapping.dev.sennetconsortium.org/",
-    "sn-integrated-mapping.dev.sennetconsortium.org/",
     "sn-integrated-mapping.sennetconsortium.org/",
     "https://sn-integrated-mapping.sennetconsortium.org/",
-    "sn-integrated-mapping.dev.sennetconsortium.org",
+    "sn-integrated-mapping.sennetconsortium.org"
 ]
