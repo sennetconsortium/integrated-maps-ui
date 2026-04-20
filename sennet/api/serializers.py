7d2d46c255b9f52e65998d4f7e17817914953689
pennycuda <141153875+pennycuda@users.noreply.github.com>
2026-04-20 16:25:05 -0400

    abort mission

--- sennet/api/serializers.py
+++ sennet/api/serializers.py
@@ -22,7 +22,6 @@ class AssaySerializer(serializers.ModelSerializer):
         model = Assay
         fields = ['assayName']
 
-
 class DatasetSerializer(serializers.Serializer):
     uuid = serializers.UUIDField(read_only=True)
     sennet_id = serializers.SerializerMethodField()
@@ -32,24 +31,20 @@ class DatasetSerializer(serializers.Serializer):
         
     annotation_metadata = serializers.JSONField(read_only=True)
 
+class OrganismSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organism
+        fields = ['organismName']
+
 class IntegratedMapSerializer(serializers.Serializer):
     integrated_map_id = serializers.UUIDField(read_only=True)
     creation_time = serializers.DateTimeField(read_only=True)
     tissue = TissueSerializer(read_only=True, many=False)
-    organism = serializers.CharField(read_only=True, many=False)
-    assay = serializers.charField(read_only=True, many=False
+    organism = OrganismSerializer(read_only=True, many=False)
     dataSets = DatasetSerializer(read_only=True, many=True)
+    assay = AssaySerializer(required=True)
     shiny_app = serializers.URLField(read_only=True)  # Add this line
     download = serializers.SerializerMethodField()
-
-
-    def get_organism(self, obj):
-        return obj.organism
-
-
-    def _assay(self, obj):
-        return obj.assay
-
     
     def get_download(self, obj):
         if obj.download is not None:
@@ -62,10 +57,7 @@ class IntegratedMapSerializer(serializers.Serializer):
         else:
             return "None"
 
-
     download_raw = serializers.SerializerMethodField()
-
-
     def get_download_raw(self, obj):
         if obj.download is not None:
             if obj.assay.assayName == "multiome-rna-atac":
