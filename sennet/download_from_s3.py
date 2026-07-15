77129b7110ecfd4051f2196e4ab02809cb4abbae
root <root@sn-integrated-mapping.sennetconsortium.org>
2026-07-15 16:26:55 +0000

    add download file

--- sennet/download_from_s3.py
+++ sennet/download_from_s3.py
@@ -20,7 +20,8 @@ def download_s3_file(file, uuid):
 
 def download_s3_files(file_list, uuid):
     for file in file_list:
-        download_s3_file(file, uuid)
+        if file:
+            download_s3_file(file, uuid)
 
 
 def download_shiny(uuid):
@@ -28,35 +29,36 @@ def download_shiny(uuid):
     os.system(f'aws s3 cp "{bucket_path}" "/opt/shiny-server/{uuid}/" --recursive')
 
 
-def get_uuid(metadata_json):
-    with open(metadata_json) as json_file:
-        metadata = json.load(json_file)
-    uuid = metadata["Data Product UUID"]
-    return uuid
-
-
 def main(
     uuid,
+    assay,
     access_key_id,
     secret_access_key,
 ):
     set_access_keys(access_key_id, secret_access_key)
     metadata = f"{uuid}.json"
-    umap = f"{uuid}.png"
+    if assay == "rna":
+        umap = f"{uuid}.png"
+        download_shiny(uuid)
+    elif assay == "multiome":
+        umap = f"{uuid}_leiden_cluster_combined.png"
+    else:
+        umap == None
     file_list = [metadata, umap]
     download_s3_files(file_list, uuid)
-    download_shiny(uuid)
 
 
 if __name__ == "__main__":
     p = ArgumentParser()
     p.add_argument("uuid", type=str)
+    p.add_argument("assay", type=str)
     p.add_argument("access_key_id", type=str)
     p.add_argument("secret_access_key", type=str)
     args = p.parse_args()
 
     main(
         args.uuid,
+        args.assay,
         args.access_key_id,
         args.secret_access_key,
     )
